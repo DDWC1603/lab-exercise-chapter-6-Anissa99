c1b3f700f287bb32ac92e7fb87dab0601e578f29
Anissa99 <33884218+Anissa99@users.noreply.github.com>
2018-01-22 10:42:04 +0800

    Exercise 2 completed

--- String-Exercise-2.cpp
+++ String-Exercise-2.cpp
@@ -1,14 +1,14 @@
-//append char1 with char2 with the right string function.
-//and display both char1 and char2
+//Nurul Anissa binti Huzaini
+//A17DW4087
 #include <iostream>
-
-int main(){
-
+#include <cstring>
+using namespace std;
+int main()
+{
 	char char1[11], char2[21];
-
 	strcpy(char1,"haillo");
 	strcpy(char2," there");
-
+    strcat(char1, char2);
 	cout<<char1<<endl;
 	cout<<char2<<endl;
 
